71d5254b255a00d57ecec12f74d05b0b5cd0a01d
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2019-08-01 13:58:07 +0100

    week 1 exercises

---
+++ Week1/000.Sandbox.java
@@ -0,0 +1,19 @@
+// This is not an exercise but a "sandbox" where you can freely test
+// whatever you want
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        // Write the code here. You can run the code by 
+        // selecting Run->Run File from the menu or by pressing Shift+F6
+        System.out.println("Trying the sandbox");
+        
+        int number = 100;
+        String animals = " dalmatians!";
+        
+        System.out.println("I have " + number + animals);
+       
+
+    }
+}
---
+++ Week1/001.Name.java
@@ -0,0 +1,9 @@
+public class Name {
+    
+    public static void main(String[] args) {
+        System.out.println("Marco Silva");
+        // the survey page is down
+
+    }
+
+}
